19d9765826879fd4cf9406ab63e1db80b6c17851
Johannes Kopton <johannes@kopton.org>
2023-11-15 10:29:01 +0100

    Add test for covariate normalization.

--- tests/test_occurence_data.py
+++ tests/test_occurence_data.py
@@ -12,3 +12,10 @@ def test_init_occurence_data():
         ), [0.48, 0.49], rtol=0.1,
     ).all()
     assert np.isclose(occuence_data.time_coords.mean(), 0.49, rtol=0.1)
+
+
+def test_covariate_normalization():
+    df["cov"] = np.random.randint(4, 17, len(df))
+    occuence_data = OccurenceData(df, space_cols=["x", "y"], time_col="time")
+    assert occuence_data._df["cov"].max() <= 1
+    assert occuence_data._df["cov"].min() >= 0

Fix residual sign.

tests/test_predictor.py
@@ -268,4 +268,4 @@ def test_kriging():
     kriging_mean, kriging_std = predictor.get_kriging_prediction(
         space, time,
     )
-    assert np.isclose(kriging_mean[0], 0.888, atol=2.0)
+    assert np.isclose(kriging_mean[0], -0.888, atol=2.0)
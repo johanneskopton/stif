dda1b4b2ab6cadcd2d0283fca96c74084f3d15b7
Johannes Kopton <johannes@kopton.org>
2024-08-06 11:10:48 +0200

    Add some tests for utils.

---
+++ tests/test_utils.py
@@ -0,0 +1,29 @@
+import numpy as np
+
+import fist.utils
+
+
+def test_distance_matrix_1d():
+    vec = np.array([1, 2, 3, 4, 5])
+    res = fist.utils.calc_distance_matrix_1d(vec)
+    assert np.allclose(
+        res,
+        np.array(
+            [
+                [0, 1, 2, 3, 4],
+                [1, 0, 1, 2, 3],
+                [2, 1, 0, 1, 2],
+                [3, 2, 1, 0, 1],
+                [4, 3, 2, 1, 0],
+            ],
+        ),
+    )
+
+
+def test_distance_matrix_2d():
+    vec = np.array([[1, 1], [2, 2], [3, 3], [4, 4], [5, 5]])
+    res = fist.utils.calc_distance_matrix_2d(vec)
+    assert np.allclose(
+        res,
+        np.linalg.norm(vec[:, None, :] - vec[None, :, :], axis=-1),
+    )
